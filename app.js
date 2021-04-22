6d6677f2d302d16525fc3db4c1cfdb50dcb620ba
rrbyn <robin.kukk23@gmail.com>
2021-04-22 10:10:49 +0300

    changes for new structure

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 function getinfo(){
     // config.js vajalik kus on spreadsheetId mis saab spreadsheeti URList ja API key mis saab google develeoperist
-    fetch('https://sheets.googleapis.com/v4/spreadsheets/' + spreadsheetId + '/values/A2:A8' + '?key=' + key)
+    fetch('https://sheets.googleapis.com/v4/spreadsheets/' + spreadsheetId + '/values/A2:E6/' + '?key=' + key + '&majorDimension=COLUMNS  ')
         .then((response) => {
         return response.json()
       })
@@ -10,9 +10,9 @@ function getinfo(){
         imageDiv = document.getElementById("images")
         console.log(data)
         let i = 0
-        for (element in data.values){
+        for (element in data.values[0]){
             let imageElement = document.createElement("img")
-            imageElement.src = data.values[i][0] 
+            imageElement.src = data.values[0][i] 
             imageDiv.appendChild(imageElement)
             i = i + 1
         }
