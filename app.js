9e3e9b6219c242d0d62c8b572feb3f1d2916c057
rrbyn <robin.kukk23@gmail.com>
2021-04-22 09:04:08 +0300

    Shows images on page

--- app.js
+++ app.js
@@ -2,15 +2,20 @@
 
 function getinfo(){
     // config.js vajalik kus on spreadsheetId mis saab spreadsheeti URList ja API key mis saab google develeoperist
-    fetch('https://sheets.googleapis.com/v4/spreadsheets/' + spreadsheetId + '/values/A1:B2' + '?key=' + key)
+    fetch('https://sheets.googleapis.com/v4/spreadsheets/' + spreadsheetId + '/values/A2:A8' + '?key=' + key)
         .then((response) => {
         return response.json()
       })
       .then((data) => {
+        imageDiv = document.getElementById("images")
         console.log(data)
-                
-               
-    
+        let i = 0
+        for (element in data.values){
+            let imageElement = document.createElement("img")
+            imageElement.src = data.values[i][0] 
+            IMAGEDiv.appendChild(imageElement)
+            i = i + 1
+        }
       });
     }
    
